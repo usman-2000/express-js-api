6e0be94db595b79ac91acaed0d1e11977590b9cc
usman-2000 <usmanrahim2000@gmail.com>
2024-08-13 00:03:38 +0500

    order schema

--- models/ecommerce/order.models.js
+++ models/ecommerce/order.models.js
@@ -1,14 +1,44 @@
 import mongoose from "mongoose";
 
+const orderItemSchema = new mongoose.Schema({
+    product:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
+    },
+    quantity:{
+        type: Number,
+        required:true
+    }
+})
+
+const billingAddressSchema = new mongoose.Schema{
+    houseNumber: {
+        type: String,
+        required:true
+    }
+}
+
 const orderSchema = new mongoose.Schema({
     orderPrice : {
         type: Number,
         required : true
     },
-    customer :{
+    buyer :{
         type: mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required: true
+    },
+    orderItems : [orderItemSchema],
+    billingAddress:{
+        type:String,
+        required: true
+    },
+    phoneNumber:{
+        type: String,
         required: true
     }
+
 },{timestamps:true})
 
 export const Order = mongoose.model('Order',orderSchema)
