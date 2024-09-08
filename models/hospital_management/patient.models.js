bafd59c16aa219c8eb1dcc1d0cca9328d60b4953
usman-2000 <usmanrahim2000@gmail.com>
2024-09-08 17:14:00 +0500

    patient data changed

--- models/hospital_management/patient.models.js
+++ models/hospital_management/patient.models.js
@@ -9,6 +9,14 @@ const patientSchema = new mongoose.Schema({
         type :Number,
         required: true
     },
+    blood_group:{
+        type: String,
+        required:true
+    },
+    gender:{
+type: String,
+enum:['M',"F",'O']
+    },
     last_disease:{
         type:String,
         required: true
